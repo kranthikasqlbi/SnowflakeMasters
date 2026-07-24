ddeb04fa85170b036749a66d98796e974ff5225e
kranthikasqlbi <kranthikasqlbi@gmail.com>
2026-07-24 14:39:06 +0530

    updated55

---
+++ Scripts/AWS_S3_CSV_JSON_Load_PBI_Report.sql
@@ -0,0 +1,100 @@
+CREATE OR REPLACE DATABASE mydatabase;
+
+--CREATE OR REPLACE TEMPORARY TABLE mycsvtable (
+-- I want to see data in powerbi, hence creating in power, temp table wonot appear in power bi 
+CREATE OR REPLACE TABLE mycsvtable (
+     id INTEGER,
+     last_name STRING,
+     first_name STRING,
+     company STRING,
+     email STRING,
+     workphone STRING,
+     cellphone STRING,
+     streetaddress STRING,
+     city STRING,
+     postalcode STRING);
+
+-- I want to see data in powerbi, hence creating in power, temp table wonot appear in power bi 
+--CREATE OR REPLACE TEMPORARY TABLE myjsontable
+CREATE OR REPLACE TABLE myjsontable (
+     json_data VARIANT);
+
+CREATE OR REPLACE WAREHOUSE mywarehouse WITH
+     WAREHOUSE_SIZE='X-SMALL'
+     AUTO_SUSPEND = 120
+     AUTO_RESUME = TRUE
+     INITIALLY_SUSPENDED=TRUE;
+
+
+
+     CREATE OR REPLACE FILE FORMAT mycsvformat
+   TYPE = 'CSV'
+   FIELD_DELIMITER = '|'
+   SKIP_HEADER = 1;
+
+   CREATE OR REPLACE FILE FORMAT myjsonformat
+  TYPE = 'JSON'
+  STRIP_OUTER_ARRAY = TRUE;
+-- STRIP..directs the COPY command to exclude the root brackets ([]) when loading data to the table.
+
+  CREATE OR REPLACE STAGE my_csv_stage
+  FILE_FORMAT = mycsvformat
+  URL = 's3://snowflake-docs';
+
+
+  CREATE OR REPLACE STAGE my_json_stage
+  FILE_FORMAT = myjsonformat
+  URL = 's3://snowflake-docs';
+
+
+
+--creating a stage that pointed to your private data files, 
+
+  /* 
+  CREATE OR REPLACE STAGE external_stage
+  FILE_FORMAT = mycsvformat
+  URL = 's3://private-bucket'
+  STORAGE_INTEGRATION = MYINT; 
+  */
+
+  --FOR SINGLE CSV FILE
+COPY INTO mycsvtable
+  FROM @my_csv_stage/tutorials/dataloading/contacts1.csv
+  ON_ERROR = 'skip_file';
+
+-- FOR MULTIPLE MATCHING CSV FILES 
+  COPY INTO mycsvtable
+  FROM @my_csv_stage/tutorials/dataloading/
+  PATTERN='.*contacts[1-5].csv'
+  ON_ERROR = 'skip_file';
+
+
+  COPY INTO myjsontable
+  FROM @my_json_stage/tutorials/dataloading/contacts.json
+  ON_ERROR = 'skip_file';
+
+
+
+CREATE OR REPLACE TABLE save_copy_errors AS SELECT * FROM TABLE(VALIDATE(mycsvtable, JOB_ID=>'<query_id>'));
+
+/*VALIDATE() is a Snowflake table function used to analyze a previous COPY INTO data load and return all row-level errors (such as invalid data types, extra columns, or malformed records) by using the Query ID of that COPY INTO job. It helps identify why rows or files failed to load so the data can be corrected and reloaded.
+*/
+
+
+  SELECT * FROM mycsvtable;
+
+  SELECT * FROM myjsontable;
+
+  DROP DATABASE IF EXISTS mydatabase;
+DROP WAREHOUSE IF EXISTS mywarehouse;
+
+CREATE OR REPLACE VIEW MYJSON_VIEW AS
+SELECT
+    JSON_DATA:customer.id::STRING AS CUSTOMER_ID,
+    JSON_DATA:customer.name.first::STRING AS FIRST_NAME,
+    JSON_DATA:customer.name.last::STRING AS LAST_NAME,
+    JSON_DATA:customer.email::STRING AS EMAIL,
+    JSON_DATA:customer.phone::STRING AS PHONE,
+    JSON_DATA:customer.company::STRING AS COMPANY,
+    JSON_DATA:customer.address::STRING AS ADDRESS
+FROM MYJSONTABLE;
--- Scripts/D2Script.sql
+++ Scripts/D2Script.sql
@@ -1,11 +1,18 @@
+--1) Creation of DB and schema 
 create database Dev_DB;
 create schema banking_schema;
 
 
-
+--2) seeing Meta data of database and schema 
 select * from information_schema.databases; 
 
 --Returns metadata about databases, that are visible to your current role(right now its Account Admin) in the Snowflake account,regardless of the current compute warehouse. 
+
+select * from information_schema.schemata; 
+
+--Returns metadata about schemas,that are visible to your current role(right now its Account Admin) in the Snowflake account,regardless of the current compute warehouse.
+
+--3) Types of DB in snowflake 
 
 --Default DB
     --1) SNOWFLAKE_LEARNING_DB : A database provided by Snowflake with tutorials, labs, and learning        resources for hands-on practice.
@@ -40,11 +47,45 @@ Testing_DB  (Prod_Environment )
 
 
 
-
-
-select * from information_schema.schemata; 
-
---Returns metadata about schemas,that are visible to your current role(right now its Account Admin) in the Snowflake account,regardless of the current compute warehouse.
-
-SELECT CURRENT_ROLE();
+--4) 
+SELECT CURRENT_ROLE();  
 SELECT CURRENT_WAREHOUSE();
+
+SELECT * FROM INFORMATION_SCHEMA.DATABASES;
+SELECT * FROM INFORMATION_SCHEMA.SCHEMAS;  --INVALID
+SELECT * FROM INFORMATION_SCHEMA.SCHEMATA;  --INVALID
+SELECT * FROM INFORMATION_SCHEMA.TABLES;
+
+SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE ='BASE TABLE';  --D1 tables
+SELECT * FROM CHANNEL; --D1 tables
+
+-- 4) Checking /running DB.schema.table in anouther db.scheme.table with below
+
+
+--this table is there in RAW.stripe as well Dev_Db.banking_Schema
+
+CREATE TABLE T1(
+ID INT
+);
+INSERT INTO T1 VALUES(1);
+SELECT * FROM T1
+
+SELECT * FROM DEV_DB.BANKING_SCHEMA.T1;
+SELECT * FROM RAW.STRIPE.T1;
+
+
+--5) 
+SHOW USERS;
+-- Ram is the team leader
+CREATE USER RAM
+PASSWORD= 'temp@123';
+
+create role analyst;
+show roles;
+
+SHOW USERS;
+
+--The default role(when new role created) of the USER is public;
+
+--6) Managaer asks to check RAM have what kind of user, role and etc etc 
+Login from another snowflake account called : RAM, loggedin with ram id and pw
